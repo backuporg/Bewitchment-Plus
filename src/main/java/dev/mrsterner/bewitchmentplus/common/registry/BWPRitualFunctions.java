3b27e160f12e2f8af71b0a08d4490d918d5a6d26
Anonymous <Sunconure11@users.noreply.github.com>
2024-03-09 15:43:28 -0500

    GOATS

--- src/main/java/dev/mrsterner/bewitchmentplus/common/registry/BWPRitualFunctions.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/registry/BWPRitualFunctions.java
@@ -4,9 +4,12 @@ import dev.mrsterner.bewitchmentplus.BewitchmentPlus;
 import dev.mrsterner.bewitchmentplus.common.ritualfunction.BindSpectralFamiliarRitualFunction;
 import dev.mrsterner.bewitchmentplus.common.ritualfunction.EffigyRitualFunction;
 import dev.mrsterner.bewitchmentplus.common.ritualfunction.SpectralFamiliarRitualFunction;
+import dev.mrsterner.bewitchmentplus.common.ritualfunction.SummonLesserDemonRitualFunction;
 import moriyashiine.bewitchment.api.registry.RitualFunction;
 import moriyashiine.bewitchment.common.entity.living.util.BWTameableEntity;
 import moriyashiine.bewitchment.common.registry.BWRegistries;
+import moriyashiine.bewitchment.common.registry.BWTags;
+import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,6 +18,7 @@ public class BWPRitualFunctions {
     public static final RitualFunction SUMMON_SPECTRAL_FAMILIAR = new SpectralFamiliarRitualFunction(ParticleTypes.HAPPY_VILLAGER, livingEntity -> livingEntity.getType().isIn(BWPTags.SACRIFICES));
     public static final RitualFunction MAKE_EFFIGY = new EffigyRitualFunction(ParticleTypes.HAPPY_VILLAGER, null);
     public static final RitualFunction SUMMON_DEATH = new BindSpectralFamiliarRitualFunction(ParticleTypes.SOUL, null);
+    public static final RitualFunction SUMMON_LESSER_DEMON = new SummonLesserDemonRitualFunction(ParticleTypes.SOUL_FIRE_FLAME, livingEntity -> livingEntity.getType().isIn(BWPTags.CAPRINES));
 
     public static <T> T register(Registry<? super T> registry, String name, T entry) {
         return Registry.register(registry, new Identifier(BewitchmentPlus.MODID, name), entry);
@@ -24,5 +28,6 @@ public class BWPRitualFunctions {
     public static void init() {
         register(BWRegistries.RITUAL_FUNCTIONS, "spectral_familiar", SUMMON_SPECTRAL_FAMILIAR);
         register(BWRegistries.RITUAL_FUNCTIONS, "make_effigy", MAKE_EFFIGY);
+        register(BWRegistries.RITUAL_FUNCTIONS, "summon_lesser_demon", SUMMON_LESSER_DEMON);
     }
 }
--- src/main/java/dev/mrsterner/bewitchmentplus/common/ritualfunction/SummonLesserDemonRitualFunction.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/ritualfunction/SummonLesserDemonRitualFunction.java
@@ -1,33 +1,28 @@
 package dev.mrsterner.bewitchmentplus.common.ritualfunction;
 
 import dev.mrsterner.bewitchmentplus.common.entity.CambionEntity;
-import dev.mrsterner.bewitchmentplus.common.entity.DrudenEntity;
-import dev.mrsterner.bewitchmentplus.common.registry.BWPTags;
+import dev.mrsterner.bewitchmentplus.common.registry.BWPEntityTypes;
 import moriyashiine.bewitchment.api.registry.RitualFunction;
-import moriyashiine.bewitchment.common.entity.living.DemonEntity;
-import moriyashiine.bewitchment.common.registry.BWEntityTypes;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.mob.BlazeEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.ParticleType;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
 
 import java.util.function.Predicate;
 
 import static net.minecraft.util.collection.Weighting.getRandom;
 
-public class SummonLesserDemonRitual extends RitualFunction {
-    public SummonLesserDemonRitual(ParticleType<?> startParticle, Predicate<LivingEntity> sacrifice) {
+public class SummonLesserDemonRitualFunction extends RitualFunction {
+    public SummonLesserDemonRitualFunction(ParticleType<?> startParticle, Predicate<LivingEntity> sacrifice) {
         super(startParticle, sacrifice);
     }
 
     public void start(ServerWorld world, BlockPos glyphPos, BlockPos effectivePos, Inventory inventory, boolean catFamiliar) {
-        DemonEntity entity = (DemonEntity)BWEntityTypes.DEMON.create(world);
+        CambionEntity entity = BWPEntityTypes.CAMBION.create(world);
         if (entity != null) {
             entity.initialize(world, world.getLocalDifficulty(effectivePos), SpawnReason.EVENT, (EntityData) null, (NbtCompound) null);
             entity.updatePositionAndAngles((double) effectivePos.getX() + 0.5, (double) effectivePos.getY(), (double) effectivePos.getZ() + 0.5, world.random.nextFloat() * 360.0F, 0.0F);
