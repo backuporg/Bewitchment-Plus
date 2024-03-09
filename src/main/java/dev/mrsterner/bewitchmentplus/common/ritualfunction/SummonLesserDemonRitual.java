37cd05751b2a9906a849133903473ee6ecf80252
Anonymous <Sunconure11@users.noreply.github.com>
2024-03-09 15:22:34 -0500

    Textures

--- src/main/java/dev/mrsterner/bewitchmentplus/common/ritualfunction/SummonLesserDemonRitual.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/ritualfunction/SummonLesserDemonRitual.java
@@ -4,7 +4,8 @@ import dev.mrsterner.bewitchmentplus.common.entity.CambionEntity;
 import dev.mrsterner.bewitchmentplus.common.entity.DrudenEntity;
 import dev.mrsterner.bewitchmentplus.common.registry.BWPTags;
 import moriyashiine.bewitchment.api.registry.RitualFunction;
-import net.minecraft.entity.Entity;
+import moriyashiine.bewitchment.common.entity.living.DemonEntity;
+import moriyashiine.bewitchment.common.registry.BWEntityTypes;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
@@ -18,23 +19,21 @@ import net.minecraft.util.math.random.Random;
 
 import java.util.function.Predicate;
 
+import static net.minecraft.util.collection.Weighting.getRandom;
+
 public class SummonLesserDemonRitual extends RitualFunction {
     public SummonLesserDemonRitual(ParticleType<?> startParticle, Predicate<LivingEntity> sacrifice) {
         super(startParticle, sacrifice);
     }
 
-
-    @Override
     public void start(ServerWorld world, BlockPos glyphPos, BlockPos effectivePos, Inventory inventory, boolean catFamiliar) {
-        LivingEntity entity;
-        Random rand;
-        if ((entity.getType().isIn(BWPTags.LESSER_DEMON))) {
-            if (entity != null) {
-                entity.initialize(world, world.getLocalDifficulty(effectivePos), SpawnReason.EVENT, (EntityData)null, (NbtCompound)null);
-                entity.updatePositionAndAngles(effectivePos.getX() + 0.5, effectivePos.getY(), effectivePos.getZ() + 0.5, world.random.nextFloat() * 360, 0);
-                world.spawnEntity();
-            }
+        DemonEntity entity = (DemonEntity)BWEntityTypes.DEMON.create(world);
+        if (entity != null) {
+            entity.initialize(world, world.getLocalDifficulty(effectivePos), SpawnReason.EVENT, (EntityData) null, (NbtCompound) null);
+            entity.updatePositionAndAngles((double) effectivePos.getX() + 0.5, (double) effectivePos.getY(), (double) effectivePos.getZ() + 0.5, world.random.nextFloat() * 360.0F, 0.0F);
+            world.spawnEntity(entity);
         }
+
         super.start(world, glyphPos, effectivePos, inventory, catFamiliar);
     }
 }
