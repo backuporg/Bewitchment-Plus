3f73f8953955ca97ff06ab4f7d6628fc194b88d1
Anonymous <Sunconure11@users.noreply.github.com>
2024-03-12 18:07:54 -0400

    Girdle of the Dryads, converted... hopefully

---
+++ src/main/java/dev/mrsterner/bewitchmentplus/client/model/GirdleOfTheDryadsArmorModel.java
@@ -0,0 +1,22 @@
+// Made with Blockbench 4.9.4
+// Exported for Minecraft version 1.17+ for Yarn
+// Paste this class into your mod and generate all required imports
+public class ModelGirdleOfTheDryadArmor extends EntityModel<Entity> {
+	
+	public ModelGirdleOfTheDryadArmor(ModelPart root) {
+		
+	}
+	public static TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
+		
+		return TexturedModelData.of(modelData, 16, 16);
+	}
+	@Override
+	public void setAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+	}
+	@Override
+	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+		
+	}
+}
